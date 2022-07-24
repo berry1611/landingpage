4627d1f06bc84389175f17cc11d6446116799088
berry1611 <berryshen16@gmail.com>
2022-07-24 23:09:29 +0700

    Add scroll to top after navigate

---
+++ client/src/components/RouterScrollToTop/index.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const RouterScrollToTop = ({ children }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <>{children}</>;
+};
+
+export default RouterScrollToTop;
--- client/src/config/Router.js
+++ client/src/config/Router.js
@@ -1,20 +1,22 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RouterScrollToTop from '../components/RouterScrollToTop';
 import { Home, Login, Register, Product, TopColorMetallic, DanaglossNC } from '../pages';
 import Cart from '../pages/Cart';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/product/topColorMetallic" element={<TopColorMetallic />} />
-        <Route path="/product/danaglossNC" element={<DanaglossNC />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/register" exact element={<Register />} />
-      </Routes>
+      <RouterScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product/topColorMetallic" element={<TopColorMetallic />} />
+          <Route path="/product/danaglossNC" element={<DanaglossNC />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/register" exact element={<Register />} />
+        </Routes>
+      </RouterScrollToTop>
     </BrowserRouter>
   );
 };
